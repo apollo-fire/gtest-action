84daf152f811502035476283079c7557ba8b051c
Tom Burley <thomas.burley@bluefruit.co.uk>
2024-01-04 15:15:35 +0000

    revert gcovr root dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,5 +35,5 @@ for i in "${paths[@]}"; do
 
   # generate a coverage report
   uuid=$(uuidgen)
-  gcovr . -r "src" --branches --xml-pretty >> "$uuid-report.xml"
+  gcovr . --branches --xml-pretty >> "$uuid-report.xml"
 done
