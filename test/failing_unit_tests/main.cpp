1a47c2f4d6206db6f41e1e61207e070785e5ef16
Grahame White <grahame.white@apollo-fire.com>
2024-05-02 10:58:05 +0100

    Added reference passing / failing unit test projects

---
+++ test/failing_unit_tests/main.cpp
@@ -0,0 +1,17 @@
+// This file should not require modification
+// It's just there to execute all test cases
+
+#include "gtest/gtest.h"
+
+
+int main(int argc, char **argv)
+
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(UnitTestValidation, Fails)
+{
+     EXPECT_EQ(true, false);
+}
---
+++ test/good_unit_tests/main.cpp
@@ -0,0 +1,17 @@
+// This file should not require modification
+// It's just there to execute all test cases
+
+#include "gtest/gtest.h"
+
+
+int main(int argc, char **argv)
+
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(UnitTestValidation, Passes)
+{
+     EXPECT_EQ(true, true);
+}
